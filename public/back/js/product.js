9a1a1634fae71b580be236f194b2274c081a2ffb
apple <874138174@qq.com>
2018-01-14 20:45:22 +0800

    push new code from cate_product.html

---
+++ public/back/js/product.js
@@ -0,0 +1,45 @@
+$(function () {
+
+  var pageNum = 1;
+  var pageSize = 5;
+  $.ajax({
+    type:'get',
+    url:'/product/queryProductDetailList',
+    data:{
+      page:pageNum,
+      pageSize:pageSize
+    },
+    success:function (info) {
+      console.log(info);
+      $('tbody').html(template('pro-tmp',info));
+    }
+  })
+
+  //模态框
+  $(".btn-addcate").on('click',function () {
+    $('.add-cate').modal('show');
+  })
+
+  //模态框下拉列表
+  $(".btn-choose").on('click',function () {
+    console.log('haha');
+    $.ajax({
+      type:'get',
+      url:'/category/querySecondCategoryPaging',
+      data:{
+        page:pageNum,
+        pageSize:pageSize
+      },
+      success:function (info) {
+        console.log(info);
+        $('#add-sec-cate').html(template('down-tmp',info));
+
+        $(".dropdown-menu li a").off().on('click',function () {
+          $('.btn-choose .text').text($(this).text());
+          $("#cate_name").val($(this).text());
+        })
+      }
+    })
+  })
+
+})
